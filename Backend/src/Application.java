fc4d4ea5ffee7f5838b210d9fc96b43286501fc4
ajith01 <ajith_me@outlook.com>
2021-04-03 11:00:07 -0400

    Revert "Just added a the basic attributes to Application class and added a Comments class, just pushing so we are all up to date"
    This reverts commit d3bb8af7703350e3e03ae60a6901c52fafa21b88.

--- Backend/src/Application.java
+++ Backend/src/Application.java
@@ -1,13 +1,3 @@
-import java.util.Date;
-
 public class Application {
-    //assumes that each application cna have at most 5 chairs and 10 committee members
-    Member[] chairs = new Member[5];
-    Member[] committeeMembers = new Member[10];
-    String position;
-    Date startingDate;
-    Date endingDate;
-    Comments comments;
-
 
 }
