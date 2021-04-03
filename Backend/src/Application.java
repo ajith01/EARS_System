d3bb8af7703350e3e03ae60a6901c52fafa21b88
ajith01 <ajith_me@outlook.com>
2021-04-03 10:34:46 -0400

    Just added a the basic attributes to Application class and added a Comments class, just pushing so we are all up to date

--- Backend/src/Application.java
+++ Backend/src/Application.java
@@ -1,3 +1,13 @@
+import java.util.Date;
+
 public class Application {
+    //assumes that each application cna have at most 5 chairs and 10 committee members
+    Member[] chairs = new Member[5];
+    Member[] committeeMembers = new Member[10];
+    String position;
+    Date startingDate;
+    Date endingDate;
+    Comments comments;
+
 
 }
