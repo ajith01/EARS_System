80a64c1e981aced69386b42f7d14a0f56f9fc981
ddamroodi <ddamroodi@algomau.ca>
2021-04-10 11:07:29 -0400

    New Class

--- src/maintest.java
+++ src/maintest.java
@@ -1,5 +1,14 @@
-public class maintest {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class maintest extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
+    }
 }
